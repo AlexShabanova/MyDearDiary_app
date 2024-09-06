4e35a68c08eff840188d20e5174b140a7408279b
AlexShabanova <iialex.shabanovaii@gmail.com>
2024-09-06 12:50:40 +0300

    create db config

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -11,9 +11,20 @@ class ApiPrefixConfig(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    # FIXME без значения по умолчанию не работает
+    # to create db engine
+    url: PostgresDsn = "postgres://postgres:postgres@localhost:5432/MyDearDiary_service"
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefixConfig = ApiPrefixConfig()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
