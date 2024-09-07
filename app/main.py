f79c03a249e165c06737a41f2ed68d18e9badb7e
AlexShabanova <iialex.shabanovaii@gmail.com>
2024-09-07 11:45:01 +0300

    add fast check of db work

--- app/main.py
+++ app/main.py
@@ -5,28 +5,27 @@ from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 from db import db_helper
+from models import Base
 
 
 # works with async context manager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
     yield
     # shutdown
     print("dispose engine")
     await db_helper.dispose()
 
 
-app = FastAPI(
+main_app = FastAPI(
     lifespan=lifespan,
 )
-app.include_router(api_router, prefix=settings.api.prefix)
-
-
-# @app.get("/")
-# def get_hello():
-#     return "Hello world!"
-
+main_app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
+    uvicorn.run(
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+    )
