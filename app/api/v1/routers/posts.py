847dbc8c38fac208c9b498a847d7b229c75cf2dd
AlexShabanova <iialex.shabanovaii@gmail.com>
2024-09-10 13:49:43 +0300

    add delete post router

--- app/api/v1/routers/posts.py
+++ app/api/v1/routers/posts.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from crud import posts as posts_crud
 from db import db_helper
 from models import Post
-from schemas.post import PostRead, PostCreate
+from schemas.post import PostRead, PostCreate, PostDelete
 
 router = APIRouter(tags=["Posts"])
 
@@ -24,4 +24,13 @@ async def create_post(
     post_data: PostCreate,
 ) -> Post:
     post = await posts_crud.create_post(session=session, post_to_create=post_data)
+    return post
+
+
+@router.delete("", response_model=PostDelete)
+async def delete_post(
+    session: Annotated[AsyncSession, Depends(db_helper.get_session)],
+    post_data: PostDelete,
+) -> Post:
+    post = await posts_crud.delete_post(session=session, post_to_delete=post_data)
     return post
