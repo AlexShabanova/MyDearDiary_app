9fb3fb0d53396cd276299588b4c8a4b2b45d2597
AlexShabanova <iialex.shabanovaii@gmail.com>
2024-09-10 13:49:04 +0300

    update post schemas

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -1,17 +1,26 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class BasePost(BaseModel):
+    pass
+
+
+class PostCreate(BasePost):
     user_id: int
     create_datetime: datetime
     update_datetime: datetime
     text: str
 
 
-class PostCreate(BasePost):
-    pass
-
-
 class PostRead(BasePost):
     id: int
+    user_id: int
+    create_datetime: datetime
+    update_datetime: datetime
+    text: str
+
+
+class PostDelete(BasePost):
+    user_id: int
+    create_datetime: datetime
