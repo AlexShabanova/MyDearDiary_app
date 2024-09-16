44952cb92865ebd76bc5a78e996f07465e8cbd88
AlexShabanova <iialex.shabanovaii@gmail.com>
2024-09-16 15:10:02 +0300

    add response and request schemas

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -3,24 +3,42 @@ from pydantic import BaseModel, ConfigDict
 
 
 class BasePost(BaseModel):
+    user_id: int
+    create_datetime: datetime
+
+
+class BasePostRequest(BasePost):
     pass
 
 
-class PostCreate(BasePost):
-    user_id: int
-    create_datetime: datetime
-    update_datetime: datetime
+class BasePostResponse(BasePost):
+    pass
+
+
+class PostCreateRequest(BasePostRequest):
     text: str
 
 
-class PostRead(BasePost):
+class PostReadRequest(BasePostRequest):
     id: int
-    user_id: int
-    create_datetime: datetime
     update_datetime: datetime
     text: str
 
 
-class PostDelete(BasePost):
-    user_id: int
-    create_datetime: datetime
+class PostDeleteRequest(BasePostResponse):
+    pass
+
+
+class PostCreateResponse(BasePostResponse):
+    update_datetime: datetime
+    text: str
+
+
+class PostReadResponse(BasePostResponse):
+    id: int
+    update_datetime: datetime
+    text: str
+
+
+class PostDeleteResponse(BasePostResponse):
+    pass
